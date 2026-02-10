991dd34e4c962242c1adfe0087851d794ab0f69a
SantiagoDevRel <santiagotrujillozuluaga@gmail.com>
2026-02-10 18:01:00 -0500

    first interaction with openai

---
+++ interact.js
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import OpenAI from "openai"; 
+
+const apiKey= process.env.OPENAI_API_KEY;
+console.log('key',apiKey)
+
+if(apiKey){
+    console.error("Missing OPEN AI KEY");
+    process.exit;
+}
+
+const client = new OpenAI({apiKey});
+
+const res = await client.responses.create({
+    model: "gpt-4o-mini",
+    input: "En español: explica qué es un LLM en 2 frases cortas."
+})
+
+console.log(`Respuesta: ${res.output_text}`)
